1f0236d367ef87279e09e4187626e6cfada821aa
Chairunisa rahma putri <116733580+chairnrp@users.noreply.github.com>
2025-03-01 14:21:39 +0700

    Perbaruan ke dua game guessme
    menambahkan library random agar angka yang masuk ke dalam variabel akan bersifat random (tidak bisa di tebak)
    
    memasukan logika if else ketika memilik nomor lubang

--- App.py
+++ App.py
@@ -1,4 +1,7 @@
+import random
+
 welcome_mesage = "Halo selamat datang di GuessME"
+gusy_location = random.randint(1,5)
 
 print("####################################")
 print("############# GuessME ##############")
@@ -8,22 +11,22 @@ gusy = """
    n n
   ('-')
   (   )╮    <--- ini gusy
- /(   )\\  
-  ^^ ^^  
+ /(   )\\
+  ^^ ^^
 """
 print(gusy)
 
 print("≽^•⩊•^≼ : Halo aku Gusy, apa kamu siap bermain? ")
 
 print("------------------------------------") # pemasukan input
-jawaban = input("{YA / TIDAK} --> ") 
+jawaban = input("{YA | TIDAK} --> ")
 
 # masuk ke pengkondisian
 
 if jawaban == "YA": # jika YA -- maka
   print("------------------------------------")
   print("≽^•⩊•^≼ : Nama mu siapa? ")
-  nama = input("Masukan nama: ")
+  nama = input("Namaku adalah... ")
   print(f" ≽^•⩊•^≼ : halo {nama}, selamat datang, kamu sudah tau aku bernama Gusy, ini adalah game tebak tebakan sederhana")
   print("≽^•⩊•^≼ : pilih tingkah kesusahan sebelum bermain! ")
 
@@ -35,56 +38,74 @@ if jawaban == "YA": # jika YA -- maka
 
   if difficulty == "EASY":
     print("≽^•⩊•^≼ : kamu memilih EASY, ayo bermain!!!!")
+    print("------------------------------------")
+    print("≽^•⩊•^≼ : sekarang perhatikan lubang di bawah ini!!")
+    print('''
+
+    |_| |_| |_| |_| |_|
+     1   2   3   4   5
+
+    di lubang manakah aku berada???
+
+    ''')
+    tebakan = input("|1|2|3|4|5| --> ")
+
+    if tebakan == gusy_location:
+      print(f"selamat {nama} kamu berhasil!!!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK}  ")
+    else:
+      print(f"ayo {nama} coba lagi!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK)}  ")
+
+######
 
   elif difficulty == "MEDIUM":
     print("≽^•⩊•^≼ : kamu memilih MEDIUM, ayo bermain!!!!")
+    print("------------------------------------")
+    print("≽^•⩊•^≼ : sekarang perhatikan lubang di bawah ini!!")
+    print('''
+
+    |_| |_| |_| |_| |_|
+     1   2   3   4   5
+
+    di lubang manakah aku berada???
+
+    ''')
+    tebakan = input("|1|2|3|4|5| --> ")
+
+    if tebakan == gusy_location:
+      print(f"selamat {nama} kamu berhasil!!!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK}  ")
+    else:
+      print(f"ayo {nama} coba lagi!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK}  ")
+
+
+######
 
   elif difficulty == "HARD":
     print("≽^•⩊•^≼ : kamu memilih HARD, wah kamu sigma bangeutt, ayo bermain!!!!")
+    print("≽^•⩊•^≼ : sekarang perhatikan lubang di bawah ini!!")
+    print('''
 
-elif jawaban == "ya": # jika ya -- maka
-  print("------------------------------------")
-  print("≽^•⩊•^≼ : Nama mu siapa? ")
-  nama = input("Masukan nama: ")
-  print(f" ≽^•⩊•^≼ : halo {nama}, selamat datang, kamu sudah tau aku bernama Gusy, ini adalah game tebak tebakan sederhana")
-  print("≽^•⩊•^≼ : pilih tingkah kesusahan sebelum bermain! ")
+    |_| |_| |_| |_| |_|
+     1   2   3   4   5
 
-  print("------------------------------------")
-  difficulty = input("{EASY | MEDIUM | HARD} ")
-  print("------------------------------------")
+    di lubang manakah aku berada???
 
-  # masuk ke percabangan lagi
+    ''')
+    tebakan = input("|1|2|3|4|5| --> ")
 
-  if difficulty == "EASY":
-    print("≽^•⩊•^≼ : kamu memilih EASY, ayo bermain!!!!")
+    if tebakan == gusy_location:
+      print(f"selamat {nama} kamu berhasil!!!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK}  ")
+    else:
+      print(f"ayo {nama} coba lagi!!!")
+      respon = input("kembali ke menu awal? {YA | TIDAK}  ")
 
-  elif difficulty == "MEDIUM":
-    print("≽^•⩊•^≼ : kamu memilih MEDIUM, ayo bermain!!!!")
 
-  elif difficulty == "HARD":
-    print("≽^•⩊•^≼ : kamu memilih HARD, wah kamu sigma bangeutt, ayo bermain!!!!")
 
-elif jawaban == "Ya": # jika Ya -- maka
-  print("------------------------------------")
-  print("≽^•⩊•^≼ : Nama mu siapa? ")
-  nama = input("Masukan nama: ")
-  print(f" ≽^•⩊•^≼ : halo {nama}, selamat datang, kamu sudah tau aku bernama Gusy, ini adalah game tebak tebakan sederhana")
-  print("≽^•⩊•^≼ : pilih tingkah kesusahan sebelum bermain! ")
-
-  print("------------------------------------")
-  difficulty = input("EASY | MEDIUM | HARD ")
-  print("------------------------------------")
-
-  # masuk ke percabangan lagi
-
-  if difficulty == "EASY":
-    print("≽^•⩊•^≼ : kamu memilih EASY, ayo bermain!!!!")
-
-  elif difficulty == "MEDIUM":
-    print("≽^•⩊•^≼ : kamu memilih MEDIUM, ayo bermain!!!!")
-
-  elif difficulty == "HARD":
-    print("≽^•⩊•^≼ : kamu memilih HARD, wah kamu sigma bangeutt, ayo bermain!!!!")
+#####
 
 else:
   print("------------------------------------")
